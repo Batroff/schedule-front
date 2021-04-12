28404d16dcbf8bb702982cb89fb4f841aaa2a2a7
Sergey Savransky <savran46@mail.ru>
2021-04-13 01:57:11 +0300

    reworked client: move to class Client, create common method request, requestJson

--- lib/http_client.dart
+++ lib/http_client.dart
@@ -2,25 +2,47 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
 
-// final _host = InternetAddress.loopbackIPv4.host;
-final _host = '10.0.2.2';
-final _port = 8080;
-HttpClient client = HttpClient();
+import 'response_decoder.dart';
 
-void main() {
-  request("БСБО-01-20");
+// DEBUG ENTRY POINT
+void main() async {
+  var res = await new Client("127.0.0.1:8080").requestJson(method: "GET", location: "", query: {"group": "БСБО-01-20"});
+  var resDecoded = deserialize(res);
+
+  var group = resDecoded.group;
+  group.showInfo();
 }
 
-Future<String> request(String group) async {
-  final _path = '/?group=$group';
-  final request = await client.get(_host, _port, _path);
-  final response = await request.close();
+class Client {
+  HttpClient client = HttpClient();
+  String baseUrl;
 
-  print(response.statusCode);
-  print(response.headers.contentType);
-  var json = '';
-  await utf8.decoder.bind(response).forEach((element) => json += element);
+  Client(String baseUrl) {
+    this.baseUrl = baseUrl;
+  }
 
-  print(json);
-  return json;
+  /// `request` takes `(method, location, {query})` params.
+  /// Returns HttpClientResponse
+  Future<HttpClientResponse> request({String method="GET", String location="", Map<String, dynamic> query}) async {
+    var uri = new Uri.http(this.baseUrl, location, query);
+    final request = await client.openUrl(method, uri);
+
+    final response = await request.close();
+
+    if (response.statusCode == 200) {
+      return response;
+    }
+    throw HttpException("Expected 200 status code, got ${response.statusCode} caused by ${response.reasonPhrase}");
+  }
+
+  /// `request` takes `(method, location, {query})` params.
+  /// Returns json
+  Future<String> requestJson({String method="GET", String location="", Map<String, dynamic> query}) async {
+    var response = await request(method: method, location: location, query: query);
+
+    var json = '';
+    await utf8.decoder.bind(response).forEach((element) => json += element);
+    return json;
+  }
 }
+
